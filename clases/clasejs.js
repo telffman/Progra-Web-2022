1c22d0accdc3ca38ce29d6a0bbca4447ebb3b670
telffman <telffman@itba.edu.ar>
2022-04-12 22:28:14 -0300

    Nuevo CSS

---
+++ clases/clasejs.js
@@ -0,0 +1,15 @@
+// descomposicion del problema
+// patrones, similitudes
+// abstracciones, 
+
+console.log("Hola mundo")
+
+/*
+Variables? Espacio en memoria donde se almacenan valores
+Que es? Un cajon
+
+Como armo una variable?
+let NombreDeLaVariable = "valor"
+= es el operador de asignacion
+var << para crear variables
+*/
